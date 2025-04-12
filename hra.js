7dea5c49ac4d0ddf7b10afba32b117f3fe2218a6
Michaela Šorelová <michaelasorelova@gmail.com>
2025-04-12 21:02:41 +0200

    rename css classes

--- hra.js
+++ hra.js
@@ -1,6 +1,6 @@
 const board = document.getElementById('game-board');
 let currentPlayer = 'circle';
-const gameSymbol = document.querySelector('.game__symbol');
+const gameSymbol = document.querySelector('.player__symbol');
 const gameIconRestart = document.querySelector('.game__icon--restart');
 
 // Vytváření tlačítek
@@ -14,8 +14,7 @@ for (let i = 1; i <= 100; i++) {
 
 // Nastavení počátečního symbolu
 
-gameSymbol.classList.add('circle'); 
-gameSymbol.classList.remove('cross');
+gameSymbol.classList.add('player__symbol--circle');
 
 
 // Nastavení posluchače událostí pro kliknutí na tlačítka
@@ -26,15 +25,15 @@ squares.forEach((square) => {
     if (event.target.disabled) return;
 
     if (currentPlayer === 'circle') {
-      event.target.classList.add('board__field--circle');
+      event.target.classList.add('game__square--circle');
       currentPlayer = 'cross'
-      gameSymbol.classList.remove('circle');
-      gameSymbol.classList.add('cross');
+      gameSymbol.classList.remove('player__symbol--circle');
+      gameSymbol.classList.add('player__symbol--cross');
     } else {
-      event.target.classList.add('board__field--cross');
+      event.target.classList.add('game__square--cross');
       currentPlayer = 'circle';
-      gameSymbol.classList.remove('cross');
-      gameSymbol.classList.add('circle');
+      gameSymbol.classList.remove('player__symbol--cross');
+      gameSymbol.classList.add('player__symbol--circle');
     }
 
     event.target.disabled = true;
